1c5f82709206dbed92789d72ac82b1e551a8899e
Burak Demiraslan <demirburak@gmail.com>
2023-07-17 18:36:34 +0300

    Completed IdentityServer config.

--- IdentityServer/FreeCourse.IdentityServer/Config.cs
+++ IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -4,6 +4,7 @@
 
 using IdentityServer4;
 using IdentityServer4.Models;
+using System;
 using System.Collections.Generic;
 
 namespace FreeCourse.IdentityServer
@@ -20,6 +21,10 @@ namespace FreeCourse.IdentityServer
         public static IEnumerable<IdentityResource> IdentityResources =>
                    new IdentityResource[]
                    {
+                       new IdentityResources.Email(),
+                       new IdentityResources.OpenId(),
+                       new IdentityResources.Profile(),
+                       new IdentityResource(){ Name = "roles", DisplayName="Roles",Description="Kullanıcı rolleri", UserClaims=new[]{"roles"}}
                    };
 
         public static IEnumerable<ApiScope> ApiScopes =>
@@ -40,6 +45,21 @@ namespace FreeCourse.IdentityServer
                     ClientSecrets = {new Secret("secret".Sha256())},
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
                     AllowedScopes = { "catalog_fullpermission", "photo_stock_fullpermission", IdentityServerConstants.LocalApi.ScopeName }
+               },
+
+               new Client
+               {
+                    ClientName = "Asp.Net Core MVC",
+                    ClientId = "WebMvcClientForUser",
+                    AllowOfflineAccess = true,
+                    ClientSecrets = {new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    AllowedScopes = {IdentityServerConstants.StandardScopes.Email, IdentityServerConstants.StandardScopes.OpenId, IdentityServerConstants.LocalApi.ScopeName,
+                       IdentityServerConstants.StandardScopes.Profile, IdentityServerConstants.StandardScopes.OfflineAccess, "roles" },
+                    AccessTokenLifetime = 1*60*60,
+                    RefreshTokenExpiration = TokenExpiration.Absolute,
+                    AbsoluteRefreshTokenLifetime = (int)(DateTime.Now.AddDays(60)-DateTime.Now).TotalSeconds,
+                    RefreshTokenUsage = TokenUsage.ReUse
                }
             };
     }
--- IdentityServer/FreeCourse.IdentityServer/Controllers/UserController.cs
+++ IdentityServer/FreeCourse.IdentityServer/Controllers/UserController.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.IIS;
+using Microsoft.IdentityModel.JsonWebTokens;
 using System.Linq;
 using System.Reflection.Metadata;
 using System.Threading.Tasks;
@@ -41,6 +43,21 @@ namespace FreeCourse.IdentityServer.Controllers
             }
 
             return NoContent(); 
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUser()
+        {
+            var userIdClaim = User.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Sub);
+
+            if (userIdClaim == null) { return BadRequest(); }
+
+            var user = await _userManager.FindByIdAsync(userIdClaim.Value);
+
+            if (user == null) { return BadRequest(); }
+
+            return Ok(new { Id = user.Id, UserName = user.UserName, Email = user.Email, City = user.City });
+
         }
     }
 }
