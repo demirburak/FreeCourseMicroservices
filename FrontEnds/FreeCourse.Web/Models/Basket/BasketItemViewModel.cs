fceb4b5bd6aaae220adaf9127f7028ca53d79588
Burak Demiraslan <demirburak@gmail.com>
2023-08-10 18:53:15 +0300

    Added basket dtos.

---
+++ FrontEnds/FreeCourse.Web/Models/Basket/BasketItemViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace FreeCourse.Web.Models.Basket
+{
+    public class BasketItemViewModel
+    {
+        public int Quantity { get; set; }
+
+        public string CourseId { get; set; }
+
+        public string CourseName { get; set; }
+
+        public decimal Price { get; set; }
+
+        private decimal? DiscountAppliedPrice { get; set; }
+
+        public decimal GetCurrentPrice() => DiscountAppliedPrice.HasValue ?  DiscountAppliedPrice.Value :  Price;
+
+        public void AppliedDiscount(decimal discountPrice)
+        {
+            DiscountAppliedPrice = discountPrice;
+        }
+    }
+}
--- FrontEnds/FreeCourse.Web/Program.cs
+++ FrontEnds/FreeCourse.Web/Program.cs
@@ -62,6 +62,7 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
 app.UseStaticFiles();
 
 app.UseRouting();
