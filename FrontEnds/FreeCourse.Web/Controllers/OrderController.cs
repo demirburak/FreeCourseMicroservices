e0774a8c4b16c483b010b8af1ddb58ab4babc5e5
Burak Demiraslan <demirburak@gmail.com>
2023-08-18 21:50:28 +0300

    Checkout history added

--- FrontEnds/FreeCourse.Web/Controllers/OrderController.cs
+++ FrontEnds/FreeCourse.Web/Controllers/OrderController.cs
@@ -57,5 +57,10 @@ namespace FreeCourse.Web.Controllers
             ViewBag.orderId = orderId;
             return View();
         }
+
+        public async Task<IActionResult> CheckoutHistory()
+        {
+            return View(await _orderService.GetOrder());
+        }
     }
 }
