4c055096a46bd2883e371832267b4079ebb2a429
Burak Demiraslan <demirburak@gmail.com>
2023-08-24 19:51:09 +0300

    Docker supports added.

--- Services/Order/FreeCourse.Services.Order.API/Program.cs
+++ Services/Order/FreeCourse.Services.Order.API/Program.cs
@@ -70,6 +70,9 @@ builder.Services.AddControllers(opt =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
