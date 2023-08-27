b8520a6c184470a0eb98c23d145b439d6b7f387b
Burak Demiraslan <demirburak@gmail.com>
2023-08-27 21:42:01 +0300

    Check

--- IdentityServer/FreeCourse.IdentityServer/Program.cs
+++ IdentityServer/FreeCourse.IdentityServer/Program.cs
@@ -60,7 +60,7 @@ namespace FreeCourse.IdentityServer
                         userManager.CreateAsync(new ApplicationUser
                         {
                             UserName = "bdemiraslan",
-                            Email = "bdemiraslan@local.com",
+                            Email = "bdemiraslan@local.com", 
                             City = "İstanbul"
                         }, "Password12*").Wait();
 
